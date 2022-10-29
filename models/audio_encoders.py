31a09175f7d8bb170a0b710514d64ba7a0fc5502
Christian Clark <christianclark687@gmail.com>
2022-10-29 19:59:04 -0400

    Added requirements.txt, vision feature extractor

--- models/audio_encoders.py
+++ models/audio_encoders.py
@@ -1,3 +1,200 @@
-# from torchvision.models import resnet50, ResNet50_Weights
+import torch
+import torch.nn as nn
+import torch.functional as F
+import torchvision
 # https://pytorch.org/vision/stable/models/resnet.html
 # https://pytorch.org/vision/stable/models.html <- how to use built in defaul vision models
+
+def get_audio_feature_vector(model: nn.Module, img_tensors: torch.Tensor, layer_dict):
+    """
+    Get feature vectors for the given audio (passed in as a spectrogram image) by returning the output
+    of the selected model at an intermediate layer.
+
+    For more info on create_feature_extractor, see the Torchvision documentation here:
+    http://pytorch.org/vision/main/generated/torchvision.models.feature_extraction.create_feature_extractor.html
+
+    Arguments
+    -----
+    model - A PyTorch nn.Module object
+    img_tensor - A batch of images stored as PyTorch tensors with dimensions (n, c, h, w)
+    layer_dict - A dictionary containing information about the desired intermediate layer output
+        Ex. {'avgpool': 'features'} to obtain the weights at ResNet's avgpool layer stored with the key "features"
+
+    Outputs
+    -----
+    feat_vec - A list of feature vectors for each of the requested layers as tuples in [("name", tensor)] format
+
+    """
+    features = []
+    model = torchvision.create_feature_extractor(model, layer_dict)
+    for n in range(img_tensors.shape[0]):
+        out = model(img_tensors[n, :, :, :])
+        feat_vec = [v for k, v in out.items()][0]
+        features.append(feat_vec)
+    features = torch.FloatTensor(features)
+    return features
+
+# Pretrained Audio Neural Networks
+def init_layer(layer):
+    """Initialize a Linear or Convolutional layer. """
+    nn.init.xavier_uniform_(layer.weight)
+ 
+    if hasattr(layer, 'bias'):
+        if layer.bias is not None:
+            layer.bias.data.fill_(0.)
+            
+    
+def init_bn(bn):
+    """Initialize a Batchnorm layer. """
+    bn.bias.data.fill_(0.)
+    bn.weight.data.fill_(1.)
+
+class ConvPreWavBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        
+        super(ConvPreWavBlock, self).__init__()
+        
+        self.conv1 = nn.Conv1d(in_channels=in_channels, 
+                              out_channels=out_channels,
+                              kernel_size=3, stride=1,
+                              padding=1, bias=False)
+                              
+        self.conv2 = nn.Conv1d(in_channels=out_channels, 
+                              out_channels=out_channels,
+                              kernel_size=3, stride=1, dilation=2, 
+                              padding=2, bias=False)
+                              
+        self.bn1 = nn.BatchNorm1d(out_channels)
+        self.bn2 = nn.BatchNorm1d(out_channels)
+
+        self.init_weight()
+        
+    def init_weight(self):
+        init_layer(self.conv1)
+        init_layer(self.conv2)
+        init_bn(self.bn1)
+        init_bn(self.bn2)
+
+        
+    def forward(self, input, pool_size):
+        
+        x = input
+        x = F.relu_(self.bn1(self.conv1(x)))
+        x = F.relu_(self.bn2(self.conv2(x)))
+        x = F.max_pool1d(x, kernel_size=pool_size)
+        
+        return x
+
+class Wavegram_Logmel_Cnn14(nn.Module):
+    def __init__(self, sample_rate, window_size, hop_size, mel_bins, fmin, 
+        fmax, classes_num):
+        
+        super(Wavegram_Logmel_Cnn14, self).__init__()
+
+        window = 'hann'
+        center = True
+        pad_mode = 'reflect'
+        ref = 1.0
+        amin = 1e-10
+        top_db = None
+
+        self.pre_conv0 = nn.Conv1d(in_channels=1, out_channels=64, kernel_size=11, stride=5, padding=5, bias=False)
+        self.pre_bn0 = nn.BatchNorm1d(64)
+        self.pre_block1 = ConvPreWavBlock(64, 64)
+        self.pre_block2 = ConvPreWavBlock(64, 128)
+        self.pre_block3 = ConvPreWavBlock(128, 128)
+        self.pre_block4 = ConvBlock(in_channels=4, out_channels=64)
+
+        # Spectrogram extractor
+        self.spectrogram_extractor = Spectrogram(n_fft=window_size, hop_length=hop_size, 
+            win_length=window_size, window=window, center=center, pad_mode=pad_mode, 
+            freeze_parameters=True)
+
+        # Logmel feature extractor
+        self.logmel_extractor = LogmelFilterBank(sr=sample_rate, n_fft=window_size, 
+            n_mels=mel_bins, fmin=fmin, fmax=fmax, ref=ref, amin=amin, top_db=top_db, 
+            freeze_parameters=True)
+
+        # Spec augmenter
+        self.spec_augmenter = SpecAugmentation(time_drop_width=64, time_stripes_num=2, 
+            freq_drop_width=8, freq_stripes_num=2)
+
+        self.bn0 = nn.BatchNorm2d(64)
+
+        self.conv_block1 = ConvBlock(in_channels=1, out_channels=64)
+        self.conv_block2 = ConvBlock(in_channels=128, out_channels=128)
+        self.conv_block3 = ConvBlock(in_channels=128, out_channels=256)
+        self.conv_block4 = ConvBlock(in_channels=256, out_channels=512)
+        self.conv_block5 = ConvBlock(in_channels=512, out_channels=1024)
+        self.conv_block6 = ConvBlock(in_channels=1024, out_channels=2048)
+
+        self.fc1 = nn.Linear(2048, 2048, bias=True)
+        self.fc_audioset = nn.Linear(2048, classes_num, bias=True)
+        
+        self.init_weight()
+
+    def init_weight(self):
+        init_layer(self.pre_conv0)
+        init_bn(self.pre_bn0)
+        init_bn(self.bn0)
+        init_layer(self.fc1)
+        init_layer(self.fc_audioset)
+ 
+    def forward(self, input, mixup_lambda=None):
+        """
+        Input: (batch_size, data_length)"""
+
+        # Wavegram
+        a1 = F.relu_(self.pre_bn0(self.pre_conv0(input[:, None, :])))
+        a1 = self.pre_block1(a1, pool_size=4)
+        a1 = self.pre_block2(a1, pool_size=4)
+        a1 = self.pre_block3(a1, pool_size=4)
+        a1 = a1.reshape((a1.shape[0], -1, 32, a1.shape[-1])).transpose(2, 3)
+        a1 = self.pre_block4(a1, pool_size=(2, 1))
+
+        # Log mel spectrogram
+        x = self.spectrogram_extractor(input)   # (batch_size, 1, time_steps, freq_bins)
+        x = self.logmel_extractor(x)    # (batch_size, 1, time_steps, mel_bins)
+        
+        x = x.transpose(1, 3)
+        x = self.bn0(x)
+        x = x.transpose(1, 3)
+
+        if self.training:
+            x = self.spec_augmenter(x)
+
+        # Mixup on spectrogram
+        if self.training and mixup_lambda is not None:
+            x = do_mixup(x, mixup_lambda)
+            a1 = do_mixup(a1, mixup_lambda)
+        
+        x = self.conv_block1(x, pool_size=(2, 2), pool_type='avg')
+
+        # Concatenate Wavegram and Log mel spectrogram along the channel dimension
+        x = torch.cat((x, a1), dim=1)
+
+        x = F.dropout(x, p=0.2, training=self.training)
+        x = self.conv_block2(x, pool_size=(2, 2), pool_type='avg')
+        x = F.dropout(x, p=0.2, training=self.training)
+        x = self.conv_block3(x, pool_size=(2, 2), pool_type='avg')
+        x = F.dropout(x, p=0.2, training=self.training)
+        x = self.conv_block4(x, pool_size=(2, 2), pool_type='avg')
+        x = F.dropout(x, p=0.2, training=self.training)
+        x = self.conv_block5(x, pool_size=(2, 2), pool_type='avg')
+        x = F.dropout(x, p=0.2, training=self.training)
+        x = self.conv_block6(x, pool_size=(1, 1), pool_type='avg')
+        x = F.dropout(x, p=0.2, training=self.training)
+        x = torch.mean(x, dim=3)
+        
+        (x1, _) = torch.max(x, dim=2)
+        x2 = torch.mean(x, dim=2)
+        x = x1 + x2
+        x = F.dropout(x, p=0.5, training=self.training)
+        x = F.relu_(self.fc1(x))
+        embedding = F.dropout(x, p=0.5, training=self.training)
+        clipwise_output = torch.sigmoid(self.fc_audioset(x))
+        
+        output_dict = {'clipwise_output': clipwise_output, 'embedding': embedding}
+
+        return output_dict
+
