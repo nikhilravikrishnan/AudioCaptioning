6d98803b89db43a3ca88190181118ccce7b0405d
nkrishnan43 <nikhilrk@gatech.edu>
2022-11-03 18:21:17 -0400

    Added dataloader for clotho dataset

---
+++ dataloaders/clotho_dataloader.py
@@ -0,0 +1,72 @@
+import torch 
+from torch.utils.data.dataloader import DataLoader
+from torch.utils.data import Dataset
+import os
+import clotho_dataset
+from transformers import RobertaTokenizer
+import numpy as np
+
+# Dataloader that takes spectrogram and caption data to serve for training us
+"""
+data_dir: Directory where the data is stored
+split: "dev" or "eva"
+tokenizer: Tokenizer to use for encoding the captions
+vocab_file: Path to the vocab file to use for encoding the captions (Optional)
+"""
+class AudioCaptioningDataset(Dataset):
+    def __init__(self, data_dir, split, tokenizer = 'roberta-base', vocab_file = None):
+        
+        SPLIT_PREFIX = f'clotho_dataset_{split}'
+        self.data_dir = os.path.join(data_dir, SPLIT_PREFIX)
+
+        # List of all the files in the dataset
+        self.dataset = os.listdir(self.data_dir)
+
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, idx):
+        
+
+        # Load the file 
+        data_item = np.load(os.path.join(self.data_dir, self.dataset[idx]), allow_pickle = True)
+        caption = data_item.caption[0]
+        spectrogram = data_item.features[0]
+        
+        # Clean the caption
+        caption = caption[5:-5]
+        
+        # Encode the caption
+        encoded_caption = RobertaTokenizer.from_pretrained('roberta-base').encode(caption)
+        encoded_caption = np.array(encoded_caption)
+        encoded_caption = torch.from_numpy(encoded_caption)
+
+        # Convert the spectrogram to a tensor
+        spectrogram = torch.from_numpy(spectrogram)
+
+        return spectrogram, encoded_caption
+
+
+
+
+
+if __name__ == "__main__":
+
+    test = AudioCaptioningDataset(data_dir = '/home/nikhilrk/MusicCaptioning/MusicCaptioning/clotho-dataset/data', split = 'dev', tokenizer = 'roberta-base')
+    print(test.__getitem__(0))
+
+
+        
+
+
+
+
+        
+
+        
+    
+        
+
+        
+        
